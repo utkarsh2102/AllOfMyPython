f555f7c7a4179c707caf4db3332f49a419280995
Utkarsh Gupta <guptautkarsh2102@gmail.com>
2018-11-24 23:49:42 +0530

    Add swap.py
    Signed-off-by: Utkarsh Gupta <guptautkarsh2102@gmail.com>

---
+++ Let's Begin/swap.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 1: "))
+
+print("After swapping, numbers are: ")
+
+num1, num2 = num2, num1                                # Sawpping, like a boss!
+
+print("Number 1 = %d" %(num1))
+print("Number 2 = %d" %(num2))
+
