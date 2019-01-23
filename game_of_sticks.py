453d8a80b265901ccc737f49e3bfd42104b9ee61
Ayush Kumar <Kumar.ayush101@gmail.com>
2019-01-23 22:32:30 +0530

    Request ammended

--- game_of_sticks.py
+++ game_of_sticks.py
@@ -1,21 +1,21 @@
-from random import randint
+#! /usr/bin/env python3
 
-"""
-This is a simple stick game, where there are 21 sticks.
-The one who picks up the last stick loses.
-You'll be playing against the computer.
-"""
+''' This is a simple stick game, where there are 21 sticks.
+    The one who picks up the last stick loses.
+    You'll be playing against the computer. '''
+
+from random import randint
 
 STICKS = 21
 
 print("There are 21 sticks, you make pick any number of stick(s) from 1-4."
-    "The one who picks the last stick loses.")
+      "The one who picks the last stick loses.")
 
 while True:
     print("{} stick(s) are left.".format(STICKS))
 
     STICKS_CHOSEN = int(input("Enter the number of stick(s) you want to pick: "))
-    if STICKS_CHOSEN not in range(1,5):
+    if STICKS_CHOSEN not in range(1, 5):
         print("Duh, wrong choice!")
         continue
     STICKS -= STICKS_CHOSEN
@@ -26,7 +26,7 @@ while True:
         print("Yay, you won! :D")
         break
 
-    CHOICE = randint(1,4)
+    CHOICE = randint(1, 4)
     print("Computer chose {} stick(s).".format(CHOICE))
     STICKS -= CHOICE
     if STICKS < 1:
